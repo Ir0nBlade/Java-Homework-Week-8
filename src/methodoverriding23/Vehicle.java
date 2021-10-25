bfd1f878dbbce7136ef1fa5faf31109b3514f89b
VimalV28 <itsvimal@gmail.com>
2021-10-25 16:14:13 +0100

    My Homework Week 8

---
+++ src/methodoverriding23/Vehicle.java
@@ -0,0 +1,17 @@
+/*
+23. Example of method overriding (Create package name ‘methodoverriding23’ and
+create all below classes in this package)
+ */
+
+package methodoverriding23;
+
+//Creating a parent class.
+
+class Vehicle {
+
+    // defining a method
+    public void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
---
+++ src/methodoverriding24/SBI.java
@@ -0,0 +1,9 @@
+package methodoverriding24;
+
+// Creating child class
+public class SBI extends Bank {
+
+    public int getRateOfInterest() {
+        return 8;
+    }
+}
